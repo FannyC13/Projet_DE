7cb2fa860f237f1a93b251ca2ffe95704b715e81
Rami <ramiwazzi@gmail.com>
2024-05-05 23:06:45 +0200

    Added kafka through docker image apache/kafka, modified IOTSimulator.scala to send items into a kafka stream instead of writing into a file

--- scala3/build.sbt
+++ scala3/build.sbt
@@ -11,5 +11,9 @@ lazy val root = project
     libraryDependencies += "org.scalameta" %% "munit" % "0.7.29" % Test,
     libraryDependencies += "org.scalatest" %% "scalatest" % "3.2.10" % Test,
     libraryDependencies += "org.json4s" %% "json4s-native" % "4.0.3",
-    libraryDependencies += "com.lihaoyi" %% "upickle" % "3.3.0"
+    libraryDependencies += "com.lihaoyi" %% "upickle" % "3.3.0",
+    libraryDependencies += "org.apache.kafka" % "kafka-clients" % "3.2.0",
+    libraryDependencies += "com.lihaoyi" %% "upickle" % "3.3.0",
+    libraryDependencies += "org.slf4j" % "slf4j-simple" % "1.7.30"
+
   )
--- scala3/src/main/scala/IOTSimulator.scala
+++ scala3/src/main/scala/IOTSimulator.scala
@@ -1,6 +1,10 @@
 import java.time.{Instant, ZoneId}
 import scala.util.Random
 import scala.io.Source
+import java.util.Properties
+import upickle.default._
+import org.apache.kafka.clients.producer.{KafkaProducer, ProducerRecord}
+
 object IoTSimulator {
   val random = new Random()
 
@@ -76,15 +80,32 @@ object IoTSimulator {
     val long = fields(4).toDouble
     IOTReport(idStudent, timestamp, sentence, lat, long)
   }
+  def createKafkaProducer(brokers: String): KafkaProducer[String, String] = {
+    val props = new Properties()
+    props.put("bootstrap.servers", brokers)
+    props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer")
+    props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer")
+    new KafkaProducer[String, String](props)
+  }
 
   def main(args: Array[String]): Unit = {
     
   
     val reports = IoTSimulator.generateRandomIoTReports(100)
+    // Kafka broker configuration - replace "localhost:9092" with your actual Kafka broker address
+    val producer = createKafkaProducer("localhost:9092")
+    val topic = "iot_reports_topic"  // Specify the Kafka topic
+
+    reports.foreach { report =>
+      val json = write(report)  // Serialize the report to JSON using uPickle
+      val record = new ProducerRecord[String, String](topic, json)
+      producer.send(record)
+    }
+
+    producer.close()
     
-    
-    val jsonFilePath = "src/ressources/SimulatorFile.json"
-    IOTReport.writeFileJSON(reports, jsonFilePath) 
+    // val jsonFilePath = "src/ressources/SimulatorFile.json"
+    // IOTReport.writeFileJSON(reports, jsonFilePath) 
   }
 }
 
